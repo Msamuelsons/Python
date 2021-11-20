04f9af2f59674d1700bf12df42572bd9c51e6b1d
Msamuelsons <marccossamuel@hotmail.com>
2021-11-20 16:24:27 -0300

    exercícios python

---
+++ ex003.py
@@ -0,0 +1,6 @@
+# Faça um programa que leia dois números e mostre a soma entre eles.
+n1 = float(input('Digite um número: '))
+n2 = float(input('Digite outro número: '))
+
+soma = n1 + n2
+print('Sua soma {}!'.format(soma))
