fde5d9d893fae7e49d1b6b03740a01b0b510516b
Msamuelsons <marccossamuel@hotmail.com>
2021-11-22 11:07:49 -0300

    exercícios python

--- ex003.py
+++ ex003.py
@@ -3,4 +3,4 @@ n1 = float(input('Digite um número: '))
 n2 = float(input('Digite outro número: '))
 
 soma = n1 + n2
-print('Sua soma {}!'.format(soma))
+print('A soma entre {} + {} = {}!'.format(n1, n2, soma))
---
+++ ex004.py
@@ -0,0 +1,12 @@
+# Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo
+# Primitivo e todas as informações possíveis sobre ela.
+
+x = input("Digite algo: ")
+print('O tipo primitivo desse valor é: {}'.format(type(x)))
+print('Só tem espaçamentos? {}'.format(x.isspace()))
+print('É um número? {}'.format(x.isnumeric()))
+print('É alfabético? {}'.format(x.isalpha()))
+print('É alfanumérico? {}'.format(x.isalnum()))
+print('Está em maiúsculas? {}'.format(x.isupper()))
+print('Está em minúsculas? {}'.format(x.islower()))
+print('Está capitalizada? {}'.format(x.istitle()))
