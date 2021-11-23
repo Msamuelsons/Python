3ce53272ddbe9156db278279033cdbbcf8cd9862
Msamuelsons <Msamuelsons>
2021-11-23 15:58:53 -0300

    Python Exercicios

--- ex004.py
+++ ex004.py
@@ -5,7 +5,7 @@
 x = input("Digite algo: ")
 print('O tipo primitivo desse valor é: {}'.format(type(x)))
 print('Só tem espaçamentos? {}'.format(x.isspace()))
-print('É um número? {}'.format(x.isnumeric()))
+print('É um número ? {}'.format(x.isnumeric()))
 print('É alfabético? {}'.format(x.isalpha()))
 print('É alfanumérico? {}'.format(x.isalnum()))
 print('Está em maiúsculas? {}'.format(x.isupper()))
